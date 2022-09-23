e43d3e29e4689c34e9aa76942fb23f6dfdac9319
manuferatu <manuferatu@gmail.com>
2022-09-23 23:00:33 +0200

    user-edit componet complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { routing, appRoutingProviders } from './app.routing';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
